45c995f45554857874dfb06bb720426847deab3e
Djordje Djukic <djukicdjole@gmail.com>
2020-10-02 02:02:22 +0200

    add raven project

---
+++ framework/src/Volo.Abp.RavenDB/Microsoft/Extensions/DependencyInjection/AbpRavenDbServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Raven.Client.Documents;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.RavenDB.Volo.Abp.RavenDB;
+
+namespace Volo.Abp.RavenDB.Microsoft.Extensions.DependencyInjection
+{
+    public static class AbpRavenDbServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRavenDbAsyncSession(
+            this IServiceCollection services,
+            RavenSettings settings)
+        {
+            var store = new DocumentStore()
+            {
+                Database = settings.DatabaseName,
+                Urls = settings.Urls,
+                Certificate = string.IsNullOrEmpty(settings.CertFilePath) ? null : new X509Certificate2(settings.CertFilePath, settings.CertPassword)
+            };
+            store.Initialize();
+            services.AddSingleton<IDocumentStore>(store);
+            return services.AddScoped(sp => sp.GetRequiredService<IDocumentStore>().OpenAsyncSession());
+        }
+    }
+}
---
+++ framework/src/Volo.Abp.RavenDB/Volo/Abp/Domain/Repositories/RavenDB/RavenDbRepository.cs
@@ -0,0 +1,109 @@
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.RavenDB.Volo.Abp.RavenDB;
+
+namespace Volo.Abp.RavenDB.Volo.Abp.Domain.Repositories.RavenDB
+{
+    public class RavenDbRepository<TRavenDbContext, TEntity> : RepositoryBase<TEntity>
+        where TRavenDbContext : IAbpRavenDbContext
+        where TEntity : class, IEntity
+    {        
+
+        private IAsyncDocumentSession Session => DbContext.AsyncSession;
+
+        public virtual TRavenDbContext DbContext => DbContextProvider.GetDbContext();        
+
+        protected IRavenDbContextProvider<TRavenDbContext> DbContextProvider { get; }
+
+        public RavenDbRepository(IRavenDbContextProvider<TRavenDbContext> dbContextProvider)
+        {
+            DbContextProvider = dbContextProvider;            
+        }
+
+        public override async Task DeleteAsync(Expression<Func<TEntity, bool>> predicate, bool autoSave = false, CancellationToken cancellationToken = default)
+        {
+            var entities = await GetQueryable()
+                .Where(predicate)
+                .ToListAsync(GetCancellationToken(cancellationToken));
+
+            foreach (var entity in entities)
+            {
+                Session.Delete(entity);
+            }
+
+            if (autoSave)
+            {
+                await Session.SaveChangesAsync(GetCancellationToken(cancellationToken));
+            }
+        }
+
+        public override async Task DeleteAsync(TEntity entity, bool autoSave = false, CancellationToken cancellationToken = default)
+        {
+            Session.Delete(entity);
+
+            if (autoSave)
+            {
+                await Session.SaveChangesAsync(GetCancellationToken(cancellationToken));
+            }
+        }
+
+        public override async Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> predicate, bool includeDetails = true, CancellationToken cancellationToken = default)
+        {
+            return await GetQueryable().Where(predicate).SingleOrDefaultAsync(GetCancellationToken(cancellationToken));
+        }
+
+        public override async Task<long> GetCountAsync(CancellationToken cancellationToken = default)
+        {
+            return await GetQueryable().CountAsync(GetCancellationToken(cancellationToken));
+        }
+
+        public override async Task<List<TEntity>> GetListAsync(bool includeDetails = false, CancellationToken cancellationToken = default)
+        {
+            return await GetQueryable().ToListAsync(GetCancellationToken(cancellationToken));
+        }
+
+        public override async Task<List<TEntity>> GetPagedListAsync(int skipCount, int maxResultCount, string sorting, bool includeDetails = false, CancellationToken cancellationToken = default)
+        {
+            return await Session.Query<TEntity>()
+                .OrderBy(sorting).PageBy(skipCount, maxResultCount)
+                .ToListAsync(GetCancellationToken(cancellationToken));
+        }
+
+        public override async Task<TEntity> InsertAsync(TEntity entity, bool autoSave = false, CancellationToken cancellationToken = default)
+        {
+            await Session.StoreAsync(entity, GetCancellationToken(cancellationToken));
+
+            if (autoSave)
+            {
+                await Session.SaveChangesAsync(GetCancellationToken(cancellationToken));
+            }
+
+            return entity;
+        }
+
+        public override async Task<TEntity> UpdateAsync(TEntity entity, bool autoSave = false, CancellationToken cancellationToken = default)
+        {
+            await Session.StoreAsync(entity, GetCancellationToken(cancellationToken));            
+
+            if (autoSave)
+            {
+                await Session.SaveChangesAsync(GetCancellationToken(cancellationToken));
+            }
+
+            return entity;
+        }
+
+        protected override IQueryable<TEntity> GetQueryable()
+        {
+            return Session.Query<TEntity>();
+        }
+    }
+}
