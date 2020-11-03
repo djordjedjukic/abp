e394d741879bf57a0e539c263a7211b525a3a9df
Djordje Djukic <djukicdjole@gmail.com>
2020-11-03 13:48:55 +0100

    test proj

---
+++ framework/src/RavenDbTest/CityRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Raven.Client.Documents.Session;
+using Volo.Abp.RavenDB.Volo.Abp.Domain.Repositories.RavenDB;
+using Volo.Abp.RavenDB.Volo.Abp.RavenDB;
+
+namespace RavenDbTest
+{
+    public class CityRepository : RavenDbRepository<ITestAppRavenDbContext, City>
+    {
+        public CityRepository(IRavenDbContextProvider<ITestAppRavenDbContext> dbContextProvider)
+            : base(dbContextProvider)
+        {
+
+        }
+
+        /*public async Task<City> FindByNameAsync(string name)
+        {
+            return await (await Collection.FindAsync(c => c.Name == name)).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Person>> GetPeopleInTheCityAsync(string cityName)
+        {
+            var city = await FindByNameAsync(cityName);
+            return await DbContext.People.AsQueryable().Where(p => p.CityId == city.Id).ToListAsync();
+        }*/
+    } 
+    
+    public interface ITestAppRavenDbContext : IAbpRavenDbContext
+    {
+        IAsyncDocumentSession AsyncSession { get; }
+    }
+    
+    public class TestAppRavenDbContext : ITestAppRavenDbContext
+    {
+        public IAsyncDocumentSession AsyncSession { get; }
+    }
+}
---
+++ framework/src/RavenDbTest/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Autofac.Core.Lifetime;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Raven.Client.Documents.Session;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.RavenDB.Volo.Abp.Domain.Repositories.RavenDB;
+using Volo.Abp.RavenDB.Volo.Abp.RavenDB;
+using Volo.Abp.RavenDB.Volo.Abp.Uow;
+using Volo.Abp.Uow;
+
+namespace RavenDbTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var serviceProvider = new ServiceCollection()
+                    //.AddSingleton<IRavenDbContextProvider<TestAppRavenDbContext>, UnitOfWorkRavenDbContextProvider<TestAppRavenDbContext>>()
+                    .AddSingleton<IConnectionStringResolver, DefaultConnectionStringResolver>()
+                    .AddSingleton<IHybridServiceScopeFactory, DefaultServiceScopeFactory>()
+                    .AddSingleton<IAmbientUnitOfWork, AmbientUnitOfWork>()
+                    .AddSingleton<IAmbientUnitOfWork, AmbientUnitOfWork>()
+                    .AddSingleton<IUnitOfWorkManager, UnitOfWorkManager>()
+                    .AddSingleton<ITestAppRavenDbContext, TestAppRavenDbContext>()
+                    .AddSingleton<IRavenDbContextProvider<ITestAppRavenDbContext>, UnitOfWorkRavenDbContextProvider<ITestAppRavenDbContext>>()
+                    .AddSingleton<IRepository<City>, CityRepository>()
+                    .BuildServiceProvider();
+                
+                //DefaultServiceScopeFactory serviceFactory = new DefaultServiceScopeFactory(new AutofacServiceProvider(null));
+                //DefaultServiceScopeFactory serviceFactory = new DefaultServiceScopeFactory(null);
+                //var uow = new AmbientUnitOfWork();
+                //var conn = new DefaultConnectionStringResolver(new OptionsManager<AbpDbConnectionOptions>());
+
+                //var contextProvider = new UnitOfWorkRavenDbContextProvider<ITestAppRavenDbContext>(new UnitOfWorkManager(uow, serviceFactory), null);
+            
+                //var userRepository = new RavenDbRepository<ITestAppRavenDbContext, User>(contextProvider);
+
+                try
+                {
+                    var cityRepository = serviceProvider.GetService<IRepository<City>>();
+                    cityRepository.InsertAsync(new City()
+                    {
+                        FirstName = "John",
+                        LastName = "Doe"
+                    });
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+
+    public class City :  AggregateRoot<string>
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+    
+    /*public interface ITestAppRavenDbContext : IAbpRavenDbContext
+    {
+        IAsyncDocumentSession AsyncSession { get; }
+    }
+    
+    public class TestAppRavenDbContext : ITestAppRavenDbContext
+    {
+        public IAsyncDocumentSession AsyncSession { get; }
+    }*/
+}
